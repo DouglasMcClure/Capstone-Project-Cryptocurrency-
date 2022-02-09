a767e2daf4e241672354661e82a415a539eb927d
DouglasMcClure <mcclur36@students.rowan.edu>
2022-02-08 22:19:37 -0500

    Added crypto candle api code.

--- Apifiles/DougApi.py
+++ Apifiles/DougApi.py
@@ -1,16 +1,20 @@
 import finnhub
 import pandas as pd
+import requests
+
+# base asset refers to the asset that is the quantity of a symbol.
+# For the symbol BTCUSDT, BTC would be the base asset.
+# quote asset refers to the asset that is the price of a symbol.
+# For the symbol BTCUSDT, USDT would be the quote asset.
 
 # Setup client
-finnhub_client = finnhub.Client(api_key="c80ac2qad3i8n3bhdjt0");
-symbol = input()
-resolution = input()
-initialTimestamp = input()
-endingTimestamp = input()
+finnhub_client = finnhub.Client(api_key="c80ac2qad3i8n3bhdjt0")
+
+r = requests.post('https://finnhub.io/api/v1/webhook/add?token=c80ac2qad3i8n3bhdjt0', json={'event': 'earnings', 'symbol': 'AAPL'})
+
 # Stock candles
 res = finnhub_client.crypto_candles(symbol, resolution, initialTimestamp, endingTimestamp)
-print(res);
+print(res)
 
-#Convert to Pandas Dataframe
-import pandas as pd
+# Convert to Pandas Dataframe
 print(pd.DataFrame(res))
