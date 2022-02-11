ef86209a9b8611f784e3d1f9c9008a0973cb43c3
hp001 <hp001@gmail.com>
2022-02-11 01:13:31 -0500

    added cryptocompare api

---
+++ Apifiles/HarshilApi.py
@@ -0,0 +1,7 @@
+import cryptocompare
+import requests
+
+cryptocompare.cryptocompare._set_api_key_parameter("cb3698c7826c7ff28f8cda185c994cccf398bcb7169a818433827ee347f45a8b")
+r = requests.get("https://min-api.cryptocompare.com/data/top/exchanges/full?fsym=BTC&tsym=USD")
+res = r.json()
+print(res)
