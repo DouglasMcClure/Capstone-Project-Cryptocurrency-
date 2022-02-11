77c39c305e02f3c22eaa7e018b4696b0a5b2890d
John Kennet <kennetjohn31@gmail.com>
2022-02-11 18:37:16 -0500

    finnhub crpytocandle api

--- Apifiles/NirjarApi.py
+++ Apifiles/NirjarApi.py
@@ -1,3 +1,4 @@
-import requests
-r = requests.get("https://api.cryptonator.com/api/full/eth-usd")
-print(r)
+import finnhub
+finnhub_client = finnhub.Client(api_key="c83f31qad3ift3bm8f7g")
+
+print(finnhub_client.crypto_candles('BINANCE:BTCUSDT', 'D', 1590988249, 1591852249))
