a417d29c3592679b96eecd2ed07c388bf5d7054a
Sebastian <castel58@students.rowan.edu>
2022-02-16 21:09:08 -0500

    coinlore api

--- Apifiles/SebastianApi.py
+++ Apifiles/SebastianApi.py
@@ -1,7 +1,6 @@
 import requests
 
-response = requests.get("https://api.coinlore.net/api/tickers/ (First 100 Coins)")
 
 response = requests.get("https://api.coinlore.net/api/coin/markets/?id=90")
-
-print(response)
+res = response.json()
+print(res)
