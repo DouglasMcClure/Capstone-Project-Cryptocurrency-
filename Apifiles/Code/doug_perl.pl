1cea4aa30520ff0d05c1c13fa97d9696e94ccda3
Douglas McClure <douglasmc115@gmail.com>
2022-02-18 16:11:01 -0500

    JSON to String, Create Output for .json files

--- Apifiles/Code/doug_perl.pl
+++ Apifiles/Code/doug_perl.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings FATAL => 'all';
 use lib qw(..);
 use JSON qw(  );
+use Data::Dumper;
 
 # create a new database in sqlite named people.
 my $dsn = "DBI:SQLite:messario.sqlite";
@@ -15,7 +16,7 @@ my $dbh = DBI->connect($dsn, \%attr);
 # check if the database opened successfully or not;
 print "Opened database successfully\n";
 
-my $filename = 'DougOutput.json';
+my $filename = "Apifiles/Code/DougOutput.json";
 # connect to and open the json file
 my $json_text = do {
    open(my $json_fh, "<:encoding(UTF-8)", $filename)
@@ -26,3 +27,31 @@ my $json_text = do {
 # store the decoded json data in a variable ($data)
 my $json = JSON->new;
 my $data = $json->decode($json_text);
+
+$dbh->do('PRAGMA foreign_keys = ON');
+$dbh->do('PRAGMA foreign_keys');
+
+my @ddl = (
+    'CREATE TABLE DATA (
+        id varchar(255),
+        title varchar(255),
+        CONTENT TEXT,
+        publish_at varchar(255),
+        name varchar(255),
+        tags varchar(255),
+        url varchar(255),
+        PRIMARY KEY (id)
+    )'
+);
+
+for my $sql (@ddl) {
+    $dbh->do($sql);
+}
+
+for ( @{$data->{data}} ) {
+   print Dumper $data;
+   my $data_id = $_->{id};
+      my $query1 ="insert into DATA (CONTENT) values (?) ";
+      my $statement = $dbh->prepare($query1);
+      $statement->execute($data_id);
+   }
