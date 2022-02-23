327976e078ecaab8937fa59c7c23030cc4765610
Douglas McClure <douglasmc115@gmail.com>
2022-02-22 19:48:30 -0500

    JSON to String, Create Output for .json files

--- Apifiles/Code/DougApi.py
+++ Apifiles/Code/DougApi.py
@@ -1,15 +1,7 @@
 import json
-
 from six.moves import urllib
 
 url = "https://data.messari.io/api/v1/news"
 res = urllib.request.urlopen(url).read()
-data = json.loads(res)
-data = json.dumps(data)
-f = open("DougOutput.json", "w")
-f.write(data)
-f.close()
-
-# open and read the file after the appending:
-f = open("DougOutput.json", "r")
-print(f.read())
+with open('DougOutput.json') as json_file:
+    data = json.load(json_file)
